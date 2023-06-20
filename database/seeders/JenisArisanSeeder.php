98a90d248f4052046ee3234dae2d392549cdfc55
wahyudedik <wdedyk@gmail.com>
2023-06-20 13:59:26 +0700

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	new file:   app/Http/Controllers/ChattingController.php
    	new file:   app/Http/Controllers/GroupArisanController.php
    	new file:   app/Http/Controllers/GroupController.php
    	new file:   app/Http/Controllers/JenisArisanController.php
    	new file:   app/Http/Requests/StoreGroupArisanRequest.php
    	new file:   app/Http/Requests/StoreGroupRequest.php
    	new file:   app/Http/Requests/StoreJenisArisanRequest.php
    	new file:   app/Http/Requests/UpdateGroupArisanRequest.php
    	new file:   app/Http/Requests/UpdateGroupRequest.php
    	new file:   app/Http/Requests/UpdateJenisArisanRequest.php
    	new file:   app/Models/Chatting.php
    	new file:   app/Models/Group.php
    	new file:   app/Models/GroupArisan.php
    	new file:   app/Models/JenisArisan.php
    	modified:   app/Models/Member.php
    	modified:   app/Models/User.php
    	new file:   app/Policies/GroupArisanPolicy.php
    	new file:   app/Policies/GroupPolicy.php
    	new file:   app/Policies/JenisArisanPolicy.php
    	new file:   database/factories/GroupArisanFactory.php
    	new file:   database/factories/GroupFactory.php
    	new file:   database/factories/JenisArisanFactory.php
    	new file:   database/seeders/GroupArisanSeeder.php
    	new file:   database/seeders/GroupSeeder.php
    	new file:   database/seeders/JenisArisanSeeder.php
    	new file:   package-lock.json
    	modified:   package.json
    	modified:   resources/views/admin/dashboard.blade.php
    	new file:   resources/views/admin/layout.blade.php
    	deleted:    resources/views/layout/admin.blade.php
    	deleted:    resources/views/layout/superadmin.blade.php
    	modified:   resources/views/superadmin/dashboard.blade.php
    	modified:   routes/web.php

---
+++ database/seeders/JenisArisanSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class JenisArisanSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
---
+++ resources/views/admin/layout.blade.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Arisan | App</title>
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome Icons -->
+    <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="dist/css/adminlte.min.css">
+    <!-- SweetAlert2 -->
+    <link rel="stylesheet" href="plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css">
+    <!-- Toastr -->
+    <link rel="stylesheet" href="plugins/toastr/toastr.min.css">
+</head>
+
+<body class="hold-transition layout-top-nav">
+    <div class="wrapper">
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <div class="content-header">
+                <div class="container">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <a href="{{ route('index') }}">
+                                <h1 class="m-0"> Arisan App <small>1.0</small></h1>
+                            </a>
+                        </div><!-- /.col -->
+                        <div class="col-sm-6">
+                            <ol class="breadcrumb float-sm-right">
+                                <li class="breadcrumb-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" role="button"
+                                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        {{ auth()->user()->name }}
+                                    </a>
+                                    <div class="dropdown-menu">
+                                        <a class="dropdown-item" href="">Profile</a>
+                                        <a class="dropdown-item" href="">Pembayaran</a>
+                                        <div class="dropdown-divider"></div>
+                                        <a class="dropdown-item" href="{{ url('/logout') }}">Logout</a>
+                                    </div>
+                                </li>
+                            </ol>
+                        </div>
+                        <!-- /.col -->
+                    </div><!-- /.row -->
+                </div><!-- /.container-fluid -->
+            </div>
+            <!-- /.content-header -->
+
+            <!-- Main content -->
+            <div class="content">
+                <div class="container">
+                    @yield('content')
+                </div>
+                <!-- /.timeline -->
+            </div>
+            <!-- /.content -->
+        </div>
+        <!-- /.content-wrapper -->
+    </div>
+    <!-- ./wrapper -->
+
+    <!-- REQUIRED SCRIPTS -->
+
+    <!-- jQuery -->
+    <script src="plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="dist/js/adminlte.min.js"></script>
+    <!-- SweetAlert2 -->
+    <script src="plugins/sweetalert2/sweetalert2.min.js"></script>
+    <!-- Toastr -->
+    <script src="plugins/toastr/toastr.min.js"></script>
+</body>
+
+</html>
